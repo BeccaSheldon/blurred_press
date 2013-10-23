0a9d4d292fd44e909725e23620da291845409796
Becca Sheldon <rebecca.e.sheldon@gmail.com>
2013-10-23 11:33:50 -0500

    finishes indi exercise

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,7 @@
 module PostsHelper
-  def post_link(post, curent_post)
-    content_tag :li, class: ('active' if post == current_post)
+  def post_link(post, current_post)
+    content_tag :li, class: ('active' if post == current_post) do
       link_to post.title, post
+    end
   end
 end
